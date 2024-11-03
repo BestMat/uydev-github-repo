b22e7d8ebce71595e116cade6589fb197f29c4a5
BestMat <76582849+BestMat@users.noreply.github.com>
2024-11-03 15:07:58 +0530

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
+import random from "./random";
 
 const app = express();
 
 app.get("/", function(req, res) {
   res.send("Hello World");
 });
+
+console.log("Random Number:", random());
